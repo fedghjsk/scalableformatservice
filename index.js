50383da60d0c4d4274880397024672d59fd6a4a3
fedghjsk <psjcrofesssor88@hotmail.com>
2024-05-11 13:26:00 +0700

    feat

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function subarraySum(nums, k) {
-  const map = new Map();
-  map.set(0, 1);
-  let count = 0;
-  let sum = 0;
-  for (const num of nums) {
-    sum += num;
-    if (map.has(sum - k)) {
-      count += map.get(sum - k);
-    }
-    map.set(sum, (map.get(sum) || 0) + 1);
-  }
-  return count;
-}
+const aliceBalanceAfter = await aptos.getAccountCoinsData({
+  accountAddress: aliceAddress,
+  minimumLedgerVersion: BigInt(response.version),
+});
+const bobBalanceAfter = await aptos.getAccountCoinsData({
+  accountAddress: bobAddress,
+});
+const sponsorBalanceAfter = await aptos.getAccountCoinsData({
+  accountAddress: sponsorAddress,
+});
